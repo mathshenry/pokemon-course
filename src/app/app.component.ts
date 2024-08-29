091c350181b4d06d408d7f2ed71efb3884d156fc
Matheus Oliveira <maoliveira@questrade.com>
2024-08-29 20:34:00 -0300

    step-3: search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -6,21 +6,26 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {PokeapiService} from "./services/pokeapi.service";
+import {AsyncPipe} from "@angular/common";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, ReactiveFormsModule],
+  imports: [RouterOutlet, HeaderComponent, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, ReactiveFormsModule, AsyncPipe, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pokemon-unifei';
-  nameControl = new FormControl('', [Validators.required]);
+  nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  pokeApiService = inject(PokeapiService)
 
   constructor() {
-    this.nameControl.valueChanges.subscribe(value => {
-      console.log(value);
-    });
+  }
+
+  fetchPokemon(){
+    this.pokeApiService.fetch(this.nameControl.value!);
   }
 }
---
+++ src/app/services/pokeapi.service.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable, signal} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {finalize} from "rxjs";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeapiService {
+  http  = inject(HttpClient);
+  pokemonData = signal<any>(null);
+  loading = signal(false);
+
+  fetch(pokemonName: string) {
+    this.loading.set(true);
+    this.pokemonData.set(null);
+    this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.replaceAll(' ', '-')}`)
+      .pipe(
+        finalize(() => this.loading.set(false))
+      )
+      .subscribe((response) => {
+      if(response){
+        this.pokemonData.set(response);
+      }
+    });
+  }
+}
